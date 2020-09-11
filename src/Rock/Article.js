8c38a71ddd97b7337600794475a47bc915292df5
Regiane Martins de Brito <martins.regiane.silva@gmail.com>
2020-09-11 13:24:03 -0300

    Changing properties names according to the database.

--- src/Rock/Article.js
+++ src/Rock/Article.js
@@ -6,17 +6,19 @@ export class Article extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0, ID_ARTICLE: 0, ARTICLE1: '', LIKE_COUNT: 0 };
+    this.state = { currentCount: 0, IdArticle: 0, ArticleText: '', LikeCount: 0 };
     this.incrementCounter = this.incrementCounter.bind(this);
 
+    debugger;  
     axios.get('http://localhost:55739/Api/Article/ArticleById?id=1')
       .then(response => {
         this.setState({ 
-          currentCount: response.data.LIKE_COUNT, 
-          ID_ARTICLE: response.data.ID_ARTICLE, 
-          ARTICLE1: response.data.ARTICLE1, 
-          LIKE_COUNT: response.data.LIKE_COUNT, 
+          currentCount: response.data.LikeCount, 
+          IdArticle: response.data.IdArticle, 
+          ArticleText: response.data.ArticleText, 
+          LikeCount: response.data.LikeCount, 
         });
+        debugger;  
       })
       .catch(function (error) {  
         console.log(error);  
@@ -25,16 +27,16 @@ export class Article extends React.Component {
 
   incrementCounter() {
     axios.post('http://localhost:55739/Api/Article/1', {
-      ID_ARTICLE: this.state.ID_ARTICLE, ARTICLE1: this.state.ARTICLE1,
-      LIKE_COUNT: this.state.LIKE_COUNT
+      IdArticle: this.state.IdArticle, ArticleText: this.state.ArticleText,
+      LikeCount: this.state.LikeCount
     })
       .then(json => {
         if (json.data.Status === 'Updated') {
           this.setState({
             currentCount: this.state.currentCount + 1,
-            ID_ARTICLE: this.state.ID_ARTICLE,
-            ARTICLE1: this.state.ARTICLE1,
-            LIKE_COUNT: this.state.LIKE_COUNT + 1
+            IdArticle: this.state.IdArticle,
+            ArticleText: this.state.ArticleText,
+            LikeCount: this.state.LikeCount + 1
           });
         }
         else {
@@ -50,8 +52,8 @@ export class Article extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <div><br />{this.state.ARTICLE1}<br /></div>
-        <button type="button" className="btn btn-primary" name="LIKE_COUNT" onChange={this.handleChange} onClick={this.incrementCounter}>Like {this.state.currentCount}</button>
+        <div><br />{this.state.ArticleText}<br /></div>
+        <button type="button" className="btn btn-primary" name="LikeCount" onChange={this.handleChange} onClick={this.incrementCounter}>Like {this.state.currentCount}</button>
       </div>
     );
   }
